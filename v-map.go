8b32c118a2c2d20b2c734b0bcd851ad0065600e9
chloyka <chloyka@gmail.com>
2025-03-28 13:22:28 -0300

    chore: rename old Map to VMap (value map)

--- v-map.go
+++ v-map.go
@@ -1,7 +1,3 @@
-// Copyright 2016 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package sync
 
 import (
@@ -9,73 +5,21 @@ import (
 	"sync/atomic"
 )
 
-// Map is like a Go map[any]*T but is safe for concurrent use
-// by multiple goroutines without additional locking or coordination.
-// Loads, stores, and deletes run in amortized constant time.
-//
-// The Map type is specialized. Most code should use a plain Go map instead,
-// with separate locking or coordination, for better type safety and to make it
-// easier to maintain other invariants along with the map content.
-//
-// The Map type is optimized for two common use cases: (1) when the entry for a given
-// key is only ever written once but read many times, as in caches that only grow,
-// or (2) when multiple goroutines read, write, and overwrite entries for disjoint
-// sets of keys. In these two cases, use of a Map may significantly reduce lock
-// contention compared to a Go map paired with a separate [Mutex] or [RWMutex].
-//
-// The zero Map is empty and ready for use. A Map must not be copied after first use.
-//
-// In the terminology of [the Go memory model], Map arranges that a write operation
-// “synchronizes before” any read operation that observes the effect of the write, where
-// read and write operations are defined as follows.
-// [Map.Load], [Map.LoadAndDelete], [Map.LoadOrStore], [Map.Swap], [Map.CompareAndSwap],
-// and [Map.CompareAndDelete] are read operations;
-// [Map.Delete], [Map.LoadAndDelete], [Map.Store], and [Map.Swap] are write operations;
-// [Map.LoadOrStore] is a write operation when it returns loaded set to false;
-// [Map.CompareAndSwap] is a write operation when it returns swapped set to true;
-// and [Map.CompareAndDelete] is a write operation when it returns deleted set to true.
-type Map[T any] struct {
+type VMap[T any] struct {
 	mu sync.Mutex
 
-	// read contains the portion of the map's contents that are safe for
-	// concurrent access (with or without mu held).
-	//
-	// The read field itself is always safe to load, but must only be stored with
-	// mu held.
-	//
-	// Entries stored in read may be updated concurrently without mu, but updating
-	// a previously-expunged entry requires that the entry be copied to the dirty
-	// map and unexpunged with mu held.
 	read atomic.Pointer[readOnly[T]]
 
-	// dirty contains the portion of the map's contents that require mu to be
-	// held. To ensure that the dirty map can be promoted to the read map quickly,
-	// it also includes all of the non-expunged entries in the read map.
-	//
-	// Expunged entries are not stored in the dirty map. An expunged entry in the
-	// clean map must be unexpunged and added to the dirty map before a new value
-	// can be stored to it.
-	//
-	// If the dirty map is nil, the next write to the map will initialize it by
-	// making a shallow copy of the clean map, omitting stale entries.
-	dirty map[any]*entry[T]
-
-	// misses counts the number of loads since the read map was last updated that
-	// needed to lock mu to determine whether the key was present.
-	//
-	// Once enough misses have occurred to cover the cost of copying the dirty
-	// map, the dirty map will be promoted to the read map (in the unamended
-	// state) and the next store to the map will make a new dirty copy.
+	dirty  map[any]*entry[T]
 	misses int
 }
 
-// readOnly is an immutable struct stored atomically in the Map.read field.
 type readOnly[T any] struct {
 	m       map[any]*entry[T]
-	amended bool // true if the dirty map contains some key not in m.
+	amended bool
 }
 
-func (m *Map[T]) loadReadOnly() readOnly[T] {
+func (m *VMap[T]) loadReadOnly() readOnly[T] {
 	if p := m.read.Load(); p != nil {
 		return *p
 	}
@@ -83,24 +27,17 @@ func (m *Map[T]) loadReadOnly() readOnly[T] {
 	return readOnly[T]{}
 }
 
-// Load returns the value stored in the map for a key, or nil if no
-// value is present.
-// The ok result indicates whether value was found in the map.
-func (m *Map[T]) Load(key any) (value any, ok bool) {
+func (m *VMap[T]) Load(key any) (value any, ok bool) {
 	read := m.loadReadOnly()
 	e, ok := read.m[key]
 	if !ok && read.amended {
 		m.mu.Lock()
-		// Avoid reporting a spurious miss if m.dirty got promoted while we were
-		// blocked on m.mu. (If further loads of the same key will not miss, it's
-		// not worth copying the dirty map for this key.)
+
 		read = m.loadReadOnly()
 		e, ok = read.m[key]
 		if !ok && read.amended {
 			e, ok = m.dirty[key]
-			// Regardless of whether the entry was present, record a miss: this key
-			// will take the slow path until the dirty map is promoted to the read
-			// map.
+
 			m.missLocked()
 		}
 		m.mu.Unlock()
@@ -112,16 +49,14 @@ func (m *Map[T]) Load(key any) (value any, ok bool) {
 	return e.load()
 }
 
-// Store sets the value for a key.
-func (m *Map[T]) Store(key any, value *T) {
+func (m *VMap[T]) Store(key any, value *T) {
 	_, _ = m.Swap(key, value)
 }
 
-// Clear deletes all the entries, resulting in an empty Map.
-func (m *Map[T]) Clear() {
+func (m *VMap[T]) Clear() {
 	read := m.loadReadOnly()
 	if len(read.m) == 0 && !read.amended {
-		// Avoid allocating a new readOnly when the map is already clear.
+
 		return
 	}
 
@@ -134,15 +69,12 @@ func (m *Map[T]) Clear() {
 	}
 
 	clear(m.dirty)
-	// Don't immediately promote the newly-cleared dirty map on the next operation.
+
 	m.misses = 0
 }
 
-// LoadOrStore returns the existing value for the key if present.
-// Otherwise, it stores and returns the given value.
-// The loaded result is true if the value was loaded, false if stored.
-func (m *Map[T]) LoadOrStore(key any, value *T) (actual any, loaded bool) {
-	// Avoid locking if it's a clean hit.
+func (m *VMap[T]) LoadOrStore(key any, value *T) (actual any, loaded bool) {
+
 	read := m.loadReadOnly()
 	if e, ok := read.m[key]; ok {
 		actual, loaded, ok := e.tryLoadOrStore(value)
@@ -163,8 +95,7 @@ func (m *Map[T]) LoadOrStore(key any, value *T) (actual any, loaded bool) {
 		m.missLocked()
 	} else {
 		if !read.amended {
-			// We're adding the first new key to the dirty map.
-			// Make sure it is allocated and mark the read-only map as incomplete.
+
 			m.dirtyLocked()
 			m.read.Store(&readOnly[T]{m: read.m, amended: true})
 		}
@@ -176,9 +107,7 @@ func (m *Map[T]) LoadOrStore(key any, value *T) (actual any, loaded bool) {
 	return actual, loaded
 }
 
-// LoadAndDelete deletes the value for a key, returning the previous value if any.
-// The loaded result reports whether the key was present.
-func (m *Map[T]) LoadAndDelete(key any) (value *T, loaded bool) {
+func (m *VMap[T]) LoadAndDelete(key any) (value *T, loaded bool) {
 	read := m.loadReadOnly()
 	e, ok := read.m[key]
 	if !ok && read.amended {
@@ -188,9 +117,7 @@ func (m *Map[T]) LoadAndDelete(key any) (value *T, loaded bool) {
 		if !ok && read.amended {
 			e, ok = m.dirty[key]
 			delete(m.dirty, key)
-			// Regardless of whether the entry was present, record a miss: this key
-			// will take the slow path until the dirty map is promoted to the read
-			// map.
+
 			m.missLocked()
 		}
 		m.mu.Unlock()
@@ -201,14 +128,11 @@ func (m *Map[T]) LoadAndDelete(key any) (value *T, loaded bool) {
 	return nil, false
 }
 
-// Delete deletes the value for a key.
-func (m *Map[T]) Delete(key any) {
+func (m *VMap[T]) Delete(key any) {
 	m.LoadAndDelete(key)
 }
 
-// Swap swaps the value for a key and returns the previous value if any.
-// The loaded result reports whether the key was present.
-func (m *Map[T]) Swap(key any, value *T) (previous *T, loaded bool) {
+func (m *VMap[T]) Swap(key any, value *T) (previous *T, loaded bool) {
 	read := m.loadReadOnly()
 	if e, ok := read.m[key]; ok {
 		if v, ok := e.trySwap(value); ok {
@@ -223,8 +147,7 @@ func (m *Map[T]) Swap(key any, value *T) (previous *T, loaded bool) {
 	read = m.loadReadOnly()
 	if e, ok := read.m[key]; ok {
 		if e.unexpungeLocked() {
-			// The entry was previously expunged, which implies that there is a
-			// non-nil dirty map and this entry is not in it.
+
 			m.dirty[key] = e
 		}
 		if v := e.swapLocked(value); v != nil {
@@ -238,8 +161,7 @@ func (m *Map[T]) Swap(key any, value *T) (previous *T, loaded bool) {
 		}
 	} else {
 		if !read.amended {
-			// We're adding the first new key to the dirty map.
-			// Make sure it is allocated and mark the read-only map as incomplete.
+
 			m.dirtyLocked()
 			m.read.Store(&readOnly[T]{m: read.m, amended: true})
 		}
@@ -249,15 +171,12 @@ func (m *Map[T]) Swap(key any, value *T) (previous *T, loaded bool) {
 	return previous, loaded
 }
 
-// CompareAndSwap swaps the old and new values for key
-// if the value stored in the map is equal to old.
-// The old value must be of a comparable type.
-func (m *Map[T]) CompareAndSwap(key, old, new *T) (swapped bool) {
+func (m *VMap[T]) CompareAndSwap(key, old, new *T) (swapped bool) {
 	read := m.loadReadOnly()
 	if e, ok := read.m[key]; ok {
 		return e.tryCompareAndSwap(old, new)
 	} else if !read.amended {
-		return false // No existing value for key.
+		return false
 	}
 
 	m.mu.Lock()
@@ -268,23 +187,15 @@ func (m *Map[T]) CompareAndSwap(key, old, new *T) (swapped bool) {
 		swapped = e.tryCompareAndSwap(old, new)
 	} else if e, ok := m.dirty[key]; ok {
 		swapped = e.tryCompareAndSwap(old, new)
-		// We needed to lock mu in order to load the entry for key,
-		// and the operation didn't change the set of keys in the map
-		// (so it would be made more efficient by promoting the dirty
-		// map to read-only).
-		// Count it as a miss so that we will eventually switch to the
-		// more efficient steady state.
+
 		m.missLocked()
 	}
 	return swapped
 }
 
-// CompareAndDelete deletes the entry for key if its value is equal to old.
-// The old value must be of a comparable type.
 //
-// If there is no current value for key in the map, CompareAndDelete
-// returns false (even if the old value is the nil interface value).
-func (m *Map[T]) CompareAndDelete(key, old *T) (deleted bool) {
+
+func (m *VMap[T]) CompareAndDelete(key, old *T) (deleted bool) {
 	read := m.loadReadOnly()
 	e, ok := read.m[key]
 	if !ok && read.amended {
@@ -293,13 +204,9 @@ func (m *Map[T]) CompareAndDelete(key, old *T) (deleted bool) {
 		e, ok = read.m[key]
 		if !ok && read.amended {
 			e, ok = m.dirty[key]
-			// Don't delete key from m.dirty: we still need to do the “compare” part
-			// of the operation. The entry will eventually be expunged when the
-			// dirty map is promoted to the read map.
+
 			//
-			// Regardless of whether the entry was present, record a miss: this key
-			// will take the slow path until the dirty map is promoted to the read
-			// map.
+
 			m.missLocked()
 		}
 		m.mu.Unlock()
@@ -316,28 +223,15 @@ func (m *Map[T]) CompareAndDelete(key, old *T) (deleted bool) {
 	return false
 }
 
-// Range calls f sequentially for each key and value present in the map.
-// If f returns false, range stops the iteration.
 //
-// Range does not necessarily correspond to any consistent snapshot of the Map's
-// contents: no key will be visited more than once, but if the value for any key
-// is stored or deleted concurrently (including by f), Range may reflect any
-// mapping for that key from any point during the Range call. Range does not
-// block other methods on the receiver; even f itself may call any method on m.
+
 //
-// Range may be O(N) with the number of elements in the map even if f returns
-// false after a constant number of calls.
-func (m *Map[T]) Range(f func(key any, value *T) bool) {
-	// We need to be able to iterate over all of the keys that were already
-	// present at the start of the call to Range.
-	// If read.amended is false, then read.m satisfies that property without
-	// requiring us to hold m.mu for a long time.
+
+func (m *VMap[T]) Range(f func(key any, value *T) bool) {
+
 	read := m.loadReadOnly()
 	if read.amended {
-		// m.dirty contains keys not in read.m. Fortunately, Range is already O(N)
-		// (assuming the caller does not break out early), so a call to Range
-		// amortizes an entire copy of the map: we can promote the dirty copy
-		// immediately!
+
 		m.mu.Lock()
 		read = m.loadReadOnly()
 		if read.amended {
@@ -361,7 +255,7 @@ func (m *Map[T]) Range(f func(key any, value *T) bool) {
 	}
 }
 
-func (m *Map[T]) missLocked() {
+func (m *VMap[T]) missLocked() {
 	m.misses++
 	if m.misses < len(m.dirty) {
 		return
@@ -371,7 +265,7 @@ func (m *Map[T]) missLocked() {
 	m.misses = 0
 }
 
-func (m *Map[T]) dirtyLocked() {
+func (m *VMap[T]) dirtyLocked() {
 	if m.dirty != nil {
 		return
 	}
